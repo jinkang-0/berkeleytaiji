9c3670ca4d94e995195d829c14cbc6d0b0d1f925
jinkang-0 <jinkang.fang@gmail.com>
2025-01-10 21:32:20 -0800

    sticky navbar, hidden on scroll

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "./navbar.module.scss";
+import { useEffect, useState } from "react";
 
 const routes = [
   {
@@ -17,9 +18,36 @@ const routes = [
 
 export default function Navbar() {
   const pathname = usePathname();
+  const [, setScrollPos] = useState(0);
+  const [hideNavbar, setHideNavbar] = useState(false);
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const scrollEvent = () => {
+      const curPos = window.scrollY;
+
+      // hide navbar if scrolling down
+      setScrollPos((prevPos) => {
+        if (prevPos > curPos) {
+          setHideNavbar(false);
+        } else {
+          setHideNavbar(true);
+        }
+
+        return curPos;
+      });
+    };
+
+    window.addEventListener("scroll", scrollEvent);
+
+    return () => {
+      window.removeEventListener("scroll", scrollEvent);
+    };
+  }, []);
 
   return (
-    <div className={styles.navbar}>
+    <div className={`${styles.navbar} ${hideNavbar ? styles.hidden : ""}`}>
       {routes.map((r) =>
         r.link === pathname ? (
           <p key={r.link}>{r.name}</p>
