139939157ac642b3ef436bc2a33d13973510bd02
jinkang-0 <jinkang.fang@gmail.com>
2025-04-13 22:52:04 -0700

    update image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("http://www.ocf.berkeley.edu/hosting-logos/*")]
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "www.ocf.berkeley.edu",
+        port: "",
+        pathname: "/hosting-logos/*"
+      }
+    ]
   }
 };
 
