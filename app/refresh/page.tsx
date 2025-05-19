6400706f0e49a597abfe5eebec8393b3f78de523
jinkang-0 <jinkang.fang@gmail.com>
2025-05-19 14:10:47 -0700

    update refresh

--- app/refresh/page.tsx
+++ app/refresh/page.tsx
@@ -2,13 +2,23 @@ import Footer from "@/components/ui/footer";
 import styles from "./page.module.scss";
 import { LinkButtonOutline } from "@/components/ui/button";
 import { revalidate } from "@/api/actions";
-import { ButtonPrimarySingle } from "@/components/ui/button-single";
+import { ButtonSinglePrimary } from "@/components/ui/button-single";
+import { getSession } from "@/api/auth";
+import { checkAdmin } from "@/api/db";
+import { notFound } from "next/navigation";
 
-export default function RefreshPage() {
+export default async function RefreshPage() {
+  const session = await getSession();
+  const user = session.success && session.user;
+  const isAdmin = user && checkAdmin(user.email);
+  if (!isAdmin) {
+    return notFound();
+  }
+
   return (
     <main className={styles.container}>
       <article>
-        <ButtonPrimarySingle
+        <ButtonSinglePrimary
           onClick={async () => {
             "use server";
             revalidate("/");
@@ -16,7 +26,7 @@ export default function RefreshPage() {
           }}
         >
           Refresh content data
-        </ButtonPrimarySingle>
+        </ButtonSinglePrimary>
         <LinkButtonOutline href="/">Back to home</LinkButtonOutline>
       </article>
       <Footer />
