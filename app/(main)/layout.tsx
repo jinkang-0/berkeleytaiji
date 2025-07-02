38b7e31abfcba042f9583aa21ce269141809c0f3
jinkang-0 <jinkang.fang@gmail.com>
2025-07-01 19:41:13 -0700

    update seo, fix refresh accessibility

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,7 +2,6 @@ import styles from "./page.module.scss";
 import Navbar from "@/components/ui/navbar";
 import Footer from "@/components/ui/footer";
 import ConditionalHero from "@/components/ui/conditional-hero";
-import AuthStatus from "@/components/auth/auth-status";
 
 export default function MainLayout({
   children
@@ -11,7 +10,6 @@ export default function MainLayout({
 }>) {
   return (
     <main className={styles.container}>
-      <AuthStatus />
       <ConditionalHero />
       <Navbar />
       {children}
