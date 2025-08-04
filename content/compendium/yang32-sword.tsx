17491c22f3209681bf3c92e634793614f66d323b
jinkang-0 <jinkang.fang@gmail.com>
2025-08-04 14:57:17 -0700

    slight modification

--- content/compendium/yang32-sword.tsx
+++ content/compendium/yang32-sword.tsx
@@ -6,10 +6,9 @@ export default function Yang32StraightSword() {
       <section>
         <p>
           The Yang 32 straight sword form is a simplified version of the
-          traditional Yang-style sword form, developed around the same time as
-          the 24-form. It covers the essential movements and techniques of the
-          Yang sword style, making it a great introductory form for those
-          interested in learning tai chi sword techniques.
+          traditional Yang-style sword form. It covers the essential movements
+          and techniques of the Yang sword style, making it a great introductory
+          form for those interested in learning tai chi sword techniques.
           <br /> <br />
           Due to its elegant and flowing movements, the Yang 32 sword form is
           often performed in competitions and demonstrations.
