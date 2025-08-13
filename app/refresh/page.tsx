41f32c06221e01b489937dd2856877ba567e0393
jinkang-0 <jinkang.fang@gmail.com>
2025-08-12 22:55:38 -0700

    update refresh path

--- app/refresh/page.tsx
+++ app/refresh/page.tsx
@@ -1,11 +1,11 @@
 import Footer from "@/components/ui/footer";
 import styles from "./page.module.scss";
 import { LinkButtonPrimary } from "@/components/ui/button";
-import { revalidatePathInternal } from "@/api/actions";
 import { ButtonSingleSecondary } from "@/components/ui/button-single";
 import { getSession } from "@/api/auth";
 import { notFound } from "next/navigation";
 import SessionRefresher from "@/components/auth/session-refresher";
+import { revalidateContent } from "@/api/actions";
 
 export default async function RefreshPage() {
   const session = await getSession();
@@ -17,24 +17,8 @@ export default async function RefreshPage() {
     <main className={styles.container}>
       <SessionRefresher user={session.user} />
       <article>
-        <ButtonSingleSecondary
-          onClick={async () => {
-            "use server";
-            await Promise.all([
-              revalidatePathInternal("/"),
-              revalidatePathInternal("/blog")
-            ]);
-          }}
-        >
+        <ButtonSingleSecondary onClick={revalidateContent}>
           Refresh content data
-        </ButtonSingleSecondary>
-        <ButtonSingleSecondary
-          onClick={async () => {
-            "use server";
-            await revalidatePathInternal("check-admin");
-          }}
-        >
-          Refresh admin list
         </ButtonSingleSecondary>
         <LinkButtonPrimary href="/">Back to home</LinkButtonPrimary>
       </article>
